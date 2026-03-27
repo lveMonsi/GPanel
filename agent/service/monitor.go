202681615dc584532d6b7d1145305096ca65cc20
lveMonsi <632063437@qq.com>
2026-03-27 14:52:20 +0800

    fix: backend dependence

--- agent/service/monitor.go
+++ agent/service/monitor.go
@@ -6,11 +6,11 @@ import (
 	"gpanel/agent/models"
 	"time"
 
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/disk"
-	"github.com/shirou/gopsutil/v3/load"
-	"github.com/shirou/gopsutil/v3/mem"
-	"github.com/shirou/gopsutil/v3/net"
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/disk"
+	"github.com/shirou/gopsutil/v4/load"
+	"github.com/shirou/gopsutil/v4/mem"
+	"github.com/shirou/gopsutil/v4/net"
 )
 
 type MonitorService struct{}
