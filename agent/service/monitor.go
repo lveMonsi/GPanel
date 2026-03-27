23fc92e6a598a9978dfd2fd20c941c77f29584ad
lveMonsi <632063437@qq.com>
2026-03-27 16:40:08 +0800

    fix: Syntax error

--- agent/service/monitor.go
+++ agent/service/monitor.go
@@ -229,69 +229,6 @@ func (s *MonitorService) CollectData() error {
 
 	return nil
 }
-	cpuPercent, _ := cpu.Percent(time.Second, false)
-	memInfo, _ := mem.VirtualMemory()
-	loadInfo, _ := load.Avg()
-	diskIO, _ := disk.IOCounters()
-	netIO, _ := net.IOCounters(false)
-
-	var diskRead, diskWrite uint64
-	for _, d := range diskIO {
-		diskRead += d.ReadBytes
-		diskWrite += d.WriteBytes
-	}
-
-	var netRecv, netSent uint64
-	if len(netIO) > 0 {
-		netRecv = netIO[0].BytesRecv
-		netSent = netIO[0].BytesSent
-	}
-
-	data := models.MonitorData{
-		Timestamp:      time.Now(),
-		CPUPercent:     cpuPercent[0],
-		MemTotal:       memInfo.Total,
-		MemUsed:        memInfo.Used,
-		MemPercent:     memInfo.UsedPercent,
-		Load1:          loadInfo.Load1,
-		Load5:          loadInfo.Load5,
-		Load15:         loadInfo.Load15,
-		DiskReadBytes:  diskRead,
-		DiskWriteBytes: diskWrite,
-		NetRecvBytes:   netRecv,
-		NetSentBytes:   netSent,
-	}
-
-	return global.DB.Create(&data).Error
-}
-
-func (s *MonitorService) GetData(startTime, endTime time.Time) ([]dto.MonitorDataResp, error) {
-	var data []models.MonitorData
-	err := global.DB.Where("timestamp BETWEEN ? AND ?", startTime, endTime).
-		Order("timestamp ASC").Find(&data).Error
-	if err != nil {
-		return nil, err
-	}
-
-	result := make([]dto.MonitorDataResp, len(data))
-	for i, d := range data {
-		result[i] = dto.MonitorDataResp{
-			Timestamp:      d.Timestamp,
-			CPUPercent:     d.CPUPercent,
-			MemTotal:       d.MemTotal,
-			MemUsed:        d.MemUsed,
-			MemPercent:     d.MemPercent,
-			Load1:          d.Load1,
-			Load5:          d.Load5,
-			Load15:         d.Load15,
-			DiskReadBytes:  d.DiskReadBytes,
-			DiskWriteBytes: d.DiskWriteBytes,
-			NetRecvBytes:   d.NetRecvBytes,
-			NetSentBytes:   d.NetSentBytes,
-		}
-	}
-	return result, nil
-}
 
 func (s *MonitorService) GetSetting() (*dto.MonitorSettingResp, error) {
 	var setting models.MonitorSetting
